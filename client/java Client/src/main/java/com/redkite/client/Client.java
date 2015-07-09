af2f8eb0c41347a8db451e425a322a4120ad0c6b
Kirill Liubun <kreol_123@ukr.net>
2015-07-10 00:00:15 +0300

    Configured logging system.

--- client/java Client/src/main/java/com/redkite/client/Client.java
+++ client/java Client/src/main/java/com/redkite/client/Client.java
@@ -1,8 +1,15 @@
 package com.redkite.client;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Kirill Liubun on 09/07/2015.
  */
 public class Client {
+
+    public static void main(String[] args) {
+
+    }
 
 }
--- server/src/main/java/com/redkite/Server.java
+++ server/src/main/java/com/redkite/Server.java
@@ -1,8 +1,15 @@
 package com.redkite;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  * Created by Kirill Liubun on 09/07/2015.
  */
 public class Server {
+    private static final Logger logger = LogManager.getLogger(Server.class);
+    public static void main(String[] args) {
+        logger.info("Server waiting for client");
+    }
 
 }
